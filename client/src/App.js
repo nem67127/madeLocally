0f0102b4239d0ef1e611ccd4a355f01aa4d95df1
Natasha Moses <nem67127@gmail.com>
2022-08-05 10:33:39 -0700

    simple header and global styled created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import HomePage from "./components/Homepage";
+import SignIn from "./components/SignIn";
+import Events from "./components/Events";
+import EventDetails from "./components/EventDetails";
+import ProfileForm from "./components/ProfileForm";
+import Profile from "./components/Profile";
+import GlobalStyles from "./GlobalStyles";
+
 const App = () => {
-  return <div> Made Locally</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/event/:eventId" element={<EventDetails />} />
+          <Route path="/profile-f/:profileId" element={<ProfileForm />} />
+          <Route path="/profile/:profileId" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,33 @@
+import styled from "styled-components";
+
 const Header = () => {
   // where logo, events link, profile link (if artisan), sign in, message icon live
+  //sign in only shows up if theres no current user
+  // and profile replaces it when there is a current user
+  return (
+    <Wrapper>
+      <Name>Made Locally</Name>
+      <Events>Events</Events>
+      <Message>MessageIcon</Message>
+      <SignIn>SignIn</SignIn>
+    </Wrapper>
+  );
 };
 export default Header;
+
+const Wrapper = styled.div`
+  height: 50px;
+  border-bottom: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 5%;
+`;
+
+const Name = styled.h1``;
+
+const SignIn = styled.div``;
+
+const Events = styled.div``;
+
+const Message = styled.div``;
