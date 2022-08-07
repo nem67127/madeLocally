34713ead679dc04355995b7d8bffd1f68c61d596
Natasha Moses <nem67127@gmail.com>
2022-08-07 12:29:44 -0700

    still not connecting to mongodb backend needs work

--- client/src/components/signin user/Authenticate.js
+++ client/src/components/signin user/Authenticate.js
@@ -1,14 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import SignUpButton from "./SignUpButton";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
 
-  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+  return isAuthenticated ? (
+    <LogoutButton />
+  ) : (
+    <Span>
+      <SignUpButton /> | <LoginButton />
+    </Span>
+  );
 };
 
 export default AuthenticationButton;
+
+const Span = styled.span``;
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//can I create two different redirects based off if they have an account or not ?
+
 root.render(
   <React.StrictMode>
     <App />
